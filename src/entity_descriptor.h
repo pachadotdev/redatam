449f2aa2c25c3b6c5de97aea8d54a3e9e16c4462
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-06-16 14:04:41 -0400

    entity descriptor header only

--- src/entity_descriptor.h
+++ src/entity_descriptor.h
@@ -1,12 +1,13 @@
 #pragma once
 
 #include <array>
+#include <boost/filesystem.hpp>
+#include <fstream>
+#include <iostream>
 #include <string>
 
 #include "primitives.h"
 #include "utils.h"
-
-#include <iostream>
 
 namespace Redatam {
 
@@ -58,10 +59,6 @@ public:
     if (!exists(real_ptr_path))
       return false;
 
-    // DEBUG
-    // std::cout << "EntityDescriptor real_ptr_path: " << real_ptr_path
-    //           << std::endl;
-
     std::fstream ptr_file(real_ptr_path.c_str(),
                           std::ios_base::in | std::ios_base::binary);
     if (!ptr_file)
@@ -72,8 +69,53 @@ public:
     return true;
   }
 
-  static EntityDescriptor fread(std::istream &stream, bool is_root);
+  static EntityDescriptor fread(std::istream &stream, bool is_root) {
+    EntityDescriptor d;
+    for (; d.name1.empty(); d.name1 = fread_string(stream)) {
+    }
+
+    if (!is_root)
+      d.name2 = fread_string(stream);
+
+    d.parent = fread_string(stream);
+    d.description = fread_string(stream);
+    d.ptr_path = fread_path(stream);
+
+    d.unknown1 = fread_uint16_t(stream);
+    d.id_varname = fread_string(stream);
+    d.name_varname = fread_string(stream);
+    d.documentation = fread_string(stream);
+    d.num_entities = fread_uint16_t(stream);
+    for (auto &c : d.unknown2)
+      c = stream.get();
+    d.num_vars = fread_uint16_t(stream);
+    d.unknown3 = fread_uint16_t(stream);
+    d.is_selectable = fread_uint16_t(stream);
+    for (auto &c : d.unknown6)
+      c = stream.get();
+    d.weight_varname = fread_string(stream);
+    d.label = fread_string(stream);
+    d.is_sensible = fread_uint16_t(stream);
+    d.sensible_threshold = fread_uint16_t(stream);
+
+    return d;
+  }
+
+  friend std::ostream &operator<<(std::ostream &stream,
+                                  const EntityDescriptor &d) {
+    return stream << "Name1: " << d.name1 << "\nName2: " << d.name2
+                  << "\nParent: " << d.parent
+                  << "\nDescription: " << d.description
+                  << "\nPTR file: " << d.ptr_path
+                  << "\nVar ID: " << d.id_varname
+                  << "\nVar Name: " << d.name_varname
+                  << "\nDocumentation: " << d.documentation
+                  << "\nNum Vars: " << d.num_vars
+                  << "\nWeighting variable: " << d.weight_varname
+                  << "\nLabel: " << d.label
+                  << "\nSelectable: " << (d.is_selectable ? "Yes" : "No")
+                  << "\nUNK2: " << d.unknown2 << "\nUNK6: " << d.unknown6;
+  }
 };
-std::ostream &operator<<(std::ostream &stream, const EntityDescriptor &d);
 
 } // namespace Redatam
