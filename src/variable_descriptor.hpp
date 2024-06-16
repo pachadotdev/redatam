3d8ae9dbc9e9365be69e770e0f6ac0a148511d3c
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-06-16 18:09:26 -0400

    refactor variable descriptor

--- src/variable_descriptor.hpp
+++ src/variable_descriptor.hpp
@@ -4,8 +4,10 @@
 #include <boost/filesystem.hpp>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <optional>
 #include <regex>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -15,12 +17,12 @@
 namespace Redatam {
 
 class VariableDescriptor {
-public:
+ public:
   class Declaration {
-  public:
+   public:
     enum class Type { BIN, CHR, DBL, INT, LNG, PCK };
-    static std::optional<Declaration>
-    fromDeclarationString(const std::string &declstr) {
+    static std::optional<Declaration> fromDeclarationString(
+        const std::string &declstr) {
       std::regex re("^(\\S+)\\s+(\\d+)$");
       std::smatch match;
       if (std::regex_search(declstr, match, re)) {
@@ -37,21 +39,15 @@ public:
   };
 
   class Descriptor {
-  public:
-    std::string alias;
+   public:
+    std::string alias, group;
     size_t decimals = 0;
-    std::string group;
-    std::optional<int> missing;
-    std::optional<int> not_applicable;
+    std::optional<int> missing, not_applicable;
   };
 
-  std::string name;
+  std::string name, filter, range, datatype, description;
   std::optional<Declaration> declaration;
-  std::string filter;
-  std::string range;
-  std::string datatype;
   std::vector<std::pair<int, std::string>> labels;
-  std::string description;
   Descriptor descriptor;
 
   uint16_t unknown1;
@@ -61,10 +57,10 @@ public:
 
   // calculated fields
   boost::filesystem::path real_rbf_path;
+
   template <typename First, typename Last>
   bool resolve_rbf_data(First first, Last last) {
-    if (!declaration or declaration->rbf_path.empty())
-      return false;
+    if (!declaration or declaration->rbf_path.empty()) return false;
 
     real_rbf_path = declaration->rbf_path;
     while (!exists(real_rbf_path) && first != last) {
@@ -115,70 +111,46 @@ public:
     d.unknown1 = fread_uint16_t(stream);
     d.documentation = fread_string(stream);
     d.id = fread_uint16_t(stream);
-    for (auto &c : d.unknown)
-      c = stream.get();
+    for (auto &c : d.unknown) c = stream.get();
     return d;
   }
 
   friend std::ostream &operator<<(std::ostream &stream,
                                   const VariableDescriptor::Declaration &d) {
-    stream << d.rbf_path << " ";
-    switch (d.type) {
-    case VariableDescriptor::Declaration::Type::BIN:
-      stream << "BIN";
-      break;
-    case VariableDescriptor::Declaration::Type::CHR:
-      stream << "CHR";
-      break;
-    case VariableDescriptor::Declaration::Type::DBL:
-      stream << "DBL";
-      break;
-    case VariableDescriptor::Declaration::Type::INT:
-      stream << "INT";
-      break;
-    case VariableDescriptor::Declaration::Type::LNG:
-      stream << "LNG";
-      break;
-    case VariableDescriptor::Declaration::Type::PCK:
-      stream << "PCK";
-      break;
-    default:
-      stream << "???";
-    }
-    return stream << " (" << d.size << ")";
+    static const std::map<Declaration::Type, std::string> typeToStr = {
+        {Declaration::Type::BIN, "BIN"}, {Declaration::Type::CHR, "CHR"},
+        {Declaration::Type::DBL, "DBL"}, {Declaration::Type::INT, "INT"},
+        {Declaration::Type::LNG, "LNG"}, {Declaration::Type::PCK, "PCK"}};
+    return stream << d.rbf_path << " " << typeToStr.at(d.type) << " (" << d.size
+                  << ")";
   }
 
   friend std::ostream &operator<<(std::ostream &stream,
-                                  const VariableDescriptor::Descriptor &d) {
-    if (!d.alias.empty())
-      stream << "ALIAS " << d.alias << " ";
-    stream << "DECIMALS " << d.decimals << " ";
-    if (!d.group.empty())
-      stream << "GROUP " << d.group << " ";
-    if (d.missing)
-      stream << " MISSING " << (*d.missing) << " ";
-    if (d.not_applicable)
-      stream << " NOTAPPLICABLE " << (*d.not_applicable);
-    return stream;
+                                  const VariableDescriptor::Descriptor &desc) {
+    return stream << "Alias: " << desc.alias << ", Group: " << desc.group
+                  << ", Decimals: " << desc.decimals << ", Missing: "
+                  << (desc.missing ? std::to_string(*desc.missing) : "N/A")
+                  << ", Not Applicable: "
+                  << (desc.not_applicable ? std::to_string(*desc.not_applicable)
+                                          : "N/A");
   }
 
   friend std::ostream &operator<<(std::ostream &stream,
                                   const VariableDescriptor &d) {
-    stream << "Name: " << d.name << "\nDeclaration: ";
+    std::stringstream ss;
+    ss << "Name: " << d.name << "\nDeclaration: ";
     if (d.declaration)
-      stream << *d.declaration;
+      ss << *d.declaration;
     else
-      stream << "NULL";
-    stream << "\nFilter: " << d.filter << "\nRange: " << d.range
-           << "\nDatatype: " << d.datatype << "\nLabels: ";
-    for (const auto &l : d.labels)
-      stream << l.second << ", ";
-    return stream << "\nDescriptor: " << d.descriptor
-                  << "\nDescription: " << d.description
-                  << "\nUNK1: " << d.unknown1
-                  << "\nDocumentation: " << d.documentation << "\nID: " << d.id
-                  << "\nUNK: " << d.unknown;
+      ss << "NULL";
+    ss << "\nFilter: " << d.filter << "\nRange: " << d.range
+       << "\nDatatype: " << d.datatype << "\nLabels: ";
+    for (const auto &l : d.labels) ss << l.second << ", ";
+    ss << "\nDescriptor: " << d.descriptor << "\nDescription: " << d.description
+       << "\nUNK1: " << d.unknown1 << "\nDocumentation: " << d.documentation
+       << "\nID: " << d.id << "\nUNK: " << d.unknown;
+    return stream << ss.str();
   }
 };
 
-} // namespace Redatam
+}  // namespace Redatam
