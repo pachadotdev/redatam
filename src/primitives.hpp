5aa8faf1598edaef7f72bafd4a07522c660c135f
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-06-16 17:57:22 -0400

    refactored primitives

--- src/primitives.hpp
+++ src/primitives.hpp
@@ -2,26 +2,32 @@
 
 #include <algorithm>
 #include <boost/filesystem.hpp>
+#include <cctype>
 #include <fstream>
 #include <string>
 #include <vector>
 
 namespace Redatam {
 
-inline uint8_t fread_uint8_t(std::istream &stream) { return stream.get(); }
+constexpr auto BYTE_MASK = 0b00111111;
+constexpr auto BYTE_PREFIX = 0b11000000;
 
-inline uint16_t fread_uint16_t(std::istream &stream) {
+inline auto fread_uint8_t(std::istream &stream) { return stream.get(); }
+
+inline auto fread_uint16_t(std::istream &stream) {
   return stream.get() + (stream.get() << 8);
 }
 
-inline uint32_t fread_uint32_t(std::istream &stream) {
+inline auto fread_uint32_t(std::istream &stream) {
   return fread_uint16_t(stream) + (fread_uint16_t(stream) << 16);
 }
 
-inline void utf8_from_windows1252(std::string &dst, unsigned char c) {
-  if (c > 0x7f) {
-    dst += static_cast<char>(0b11000000 | ((c & 0b11000000) >> 6));
-    dst += static_cast<char>(0b10000000 | (c & 0b00111111));
+inline void utf8_from_windows1252(std::string &dst, std::byte c) {
+  if (static_cast<uint8_t>(c) > 0x7f) {
+    dst += static_cast<char>(BYTE_PREFIX |
+                             ((static_cast<uint8_t>(c) & BYTE_MASK) >> 6));
+    dst +=
+        static_cast<char>(0b10000000 | (static_cast<uint8_t>(c) & BYTE_MASK));
   } else {
     dst += static_cast<char>(c);
   }
@@ -30,57 +36,53 @@ inline void utf8_from_windows1252(std::string &dst, unsigned char c) {
 inline std::string fread_string(std::istream &stream) {
   size_t sz = fread_uint16_t(stream);
   std::string result;
-  if (sz == 0)
-    return result;
-  if (sz == 0xFFFF)
-    sz = fread_uint32_t(stream);
+  if (sz == 0) return result;
+  if (sz == 0xFFFF) sz = fread_uint32_t(stream);
   result.reserve(sz);
   for (size_t i = 0; i < sz; ++i)
-    utf8_from_windows1252(result, stream.get());
+    utf8_from_windows1252(result, static_cast<std::byte>(stream.get()));
   return result;
 }
 
 inline boost::filesystem::path fread_path(std::istream &stream) {
-  std::string raw = fread_string(stream);
-  std::replace(begin(raw), end(raw), '\\',
-               boost::filesystem::path::preferred_separator);
+  auto raw = fread_string(stream);
+  std::transform(begin(raw), end(raw), begin(raw), [](char c) {
+    return c == '\\' ? boost::filesystem::path::preferred_separator : c;
+  });
   return raw;
 }
 
 inline std::string fread_fixed_string(std::istream &stream, size_t width) {
   std::string result;
   while (width > 0) {
-    int c = stream.get();
-    utf8_from_windows1252(result, c);
+    auto c = stream.get();
+    utf8_from_windows1252(result, static_cast<std::byte>(c));
     --width;
   }
-  int i = result.size() - 1;
-  while (i >= 0 && std::isspace(result[i]))
-    --i;
+  auto i = result.size() - 1;
+  while (i >= 0 && std::isspace(result[i])) --i;
   result.resize(i + 1);
   return result;
 }
 
-inline size_t fread_decimal(std::istream &stream, size_t digits) {
+inline auto fread_decimal(std::istream &stream, size_t digits) {
   size_t result = 0;
   for (size_t i = 0; i < digits; ++i)
     result = result * 10 + (fread_uint8_t(stream) - '0');
   return result;
 }
 
-inline double fread_double(std::istream &stream) {
+inline auto fread_double(std::istream &stream) {
   double d;
-  stream.read((char *)&d, sizeof(double));
+  stream.read(reinterpret_cast<char *>(&d), sizeof(double));
   return d;
 }
 
-inline uint32_t fread_PCK(std::istream &stream) {
-  return fread_uint32_t(stream);
-}
+inline auto fread_PCK(std::istream &stream) { return fread_uint32_t(stream); }
 
-inline uint32_t fread_BIN(std::istream &stream) {
+inline auto fread_BIN(std::istream &stream) {
   return (stream.get() << 24) + (stream.get() << 16) + (stream.get() << 8) +
          stream.get();
 }
 
-} // namespace Redatam
+}  // namespace Redatam
