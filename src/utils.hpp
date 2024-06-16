c6fa488b4fa2c767ac5c0468680abfae3330110e
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-06-16 17:57:34 -0400

    refactor utils

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <array>
 #include <boost/filesystem.hpp>
 #include <fstream>
@@ -9,28 +10,29 @@
 namespace Redatam {
 
 inline std::string tolower(const std::string &s) {
-  std::string r;
-  r.reserve(s.size());
-  std::transform(begin(s), end(s), back_inserter(r),
-                 [](char ch) { return std::tolower(ch); });
+  std::string r(s);
+  std::transform(r.begin(), r.end(), r.begin(), ::tolower);
   return r;
 }
 
-inline boost::filesystem::path
-locate_icase(const boost::filesystem::path &path) {
+inline boost::filesystem::path locate_icase(
+    const boost::filesystem::path &path) {
   std::string base_lower = tolower(path.filename().string());
-  for (auto &p : boost::filesystem::directory_iterator(path.parent_path()))
-    if (base_lower == tolower(p.path().filename().string()))
-      return path.parent_path() / p.path().filename();
-  return "";
+  auto it =
+      std::find_if(boost::filesystem::directory_iterator(path.parent_path()),
+                   {}, [&](const auto &p) {
+                     return base_lower == tolower(p.path().filename().string());
+                   });
+  return it != boost::filesystem::directory_iterator()
+             ? it->path()
+             : boost::filesystem::path();
 }
 
 template <typename T, size_t N>
 std::ostream &operator<<(std::ostream &stream, const std::array<T, N> &arr) {
-  for (int c : arr)
-    stream << "0x" << std::setbase(16) << std::setw(2) << std::setfill('0') << c
-           << " ";
-  return stream << std::setbase(10) << std::setfill('0');
+  for (const auto &c : arr)
+    stream << "0x" << std::hex << std::setw(2) << std::setfill('0') << c << " ";
+  return stream << std::dec << std::setfill('0');
 }
 
-} // namespace Redatam
+}  // namespace Redatam
